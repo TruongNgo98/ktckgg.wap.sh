0f11c7275e0c9c3ae06402bc311cb9f4252ae35a
TruongNgo98 <truongngo1998@gmail.com>
2024-12-29 11:08:14 +0700

    Thêm module tính IP, điều chỉnh clone text cho phép nhập dữ liệu chữ trong index, điều chỉnh giao diện chung.

--- load-header.js
+++ load-header.js
@@ -9,10 +9,13 @@ document.addEventListener("DOMContentLoaded", function () {
               <a class="nav-link" href="index.html">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="csv-to-json.html">CSV to JSON</a>
+              <a class="nav-link" href="ip-calculator.html">IP calculator</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="clone-text.html">Clone text</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="csv-to-json.html">CSV to JSON</a>
             </li>
           </ul>
         </div>
